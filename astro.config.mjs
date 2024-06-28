4e148e44846f681eb3b32a1404769eeececfcb81
verdulife <verdu@live.com>
2024-06-28 23:21:22 +0200

    welcome styles 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
